5083d8dcf70eb3f08ae3b6fc519b21fd95d706e9
Chiemerie <chiemerieeze19@gmail.com>
2026-03-29 01:28:47 -0400

    Camera has been set up to scan barcodes

---
+++ test.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.camera import Camera
+
+class Cam(Camera):
+    def __init__(self, **kwargs):
+        # super().__init__(**kwargs)
+        self.play = True
+
+# class lab(Label):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.text = "HALLO"
+
+class MyApp(App):
+
+    def build(self):
+        return Cam()
+    
+
+if __name__ == '__main__':
+    MyApp().run()
